bb639fe60eb97f96632d8839243054461c2210c8
GuiAndrew <guiandrew.pt@gmail.com>
2020-03-28 08:38:58 +0000

    last

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -10,7 +10,7 @@ const About = () => {
                     <div className="card blue-grey">
                         <div className="card-content white-text">
                             <h1>About</h1>
-                            <p>This is an app game. This was 'cooke' to the Wild Code School course.</p>
+                            <p>This is an app game. This was 'cooke' to the Wild Code School course by GuiAndrew</p>
                             <p>This game app is based on the board game Bananagrams, wherein lettered tiles are used to spell words.</p>
                             <p>All titles form anime and manga came from the Studio Ghibli API, and from MyAnimeList.</p>
                             <p>You can see the Wikipedia <a target="_blank" href="https://en.wikipedia.org/wiki/Bananagrams" rel="noopener noreferrer">here</a></p>
