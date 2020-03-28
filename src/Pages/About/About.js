212dae84e4d724bc10e2e9c4a28f399617332471
GuiAndrew <guiandrew.pt@gmail.com>
2020-03-28 08:44:11 +0000

    correct enghlish words in about component

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -12,7 +12,7 @@ const About = () => {
                             <h1>About</h1>
                             <p>This is an app game. This was 'cooke' to the Wild Code School course by GuiAndrew</p>
                             <p>This game app is based on the board game Bananagrams, wherein lettered tiles are used to spell words.</p>
-                            <p>All titles form anime and manga came from the Studio Ghibli API, and from MyAnimeList.</p>
+                            <p>All titles from anime and manga came from the Studio Ghibli API, and from MyAnimeList.</p>
                             <p>You can see the Wikipedia <a target="_blank" href="https://en.wikipedia.org/wiki/Bananagrams" rel="noopener noreferrer">here</a></p>
                         </div>
                     </div>
